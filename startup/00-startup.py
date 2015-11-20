bd2728d70f39681404739dfcba4ef93f5c7321b2
hhslepicka <hhslepicka@gmail.com>
2015-11-20 15:18:17 -0500

    Initial Commit

---
+++ startup/00-startup.py
@@ -0,0 +1,16 @@
+import logging
+
+session_mgr._logger.setLevel(logging.INFO)
+# To help with debugging scanning-related problems, uncomment the following:
+# session_mgr._logger.setLevel(logging.DEBUG)
+
+handler = logging.StreamHandler(sys.stderr)
+fmt = logging.Formatter("%(asctime)-15s [%(name)5s:%(levelname)s] %(message)s")
+handler.setFormatter(fmt)
+
+from ophyd.commands import *
+from dataportal import (DataBroker as db,
+                        StepScan as ss,
+                        DataBroker, StepScan,
+                        DataMuxer)
+
