fed23d0ec63959ec1ff4b1768cfce2b7ca4ec391
Julien Lhermitte <lhermitte@bnl.gov>
2018-01-18 11:25:35 -0500

    Added plotting instructions on 90-plans.py

--- startup/90-plans.py
+++ startup/90-plans.py
@@ -53,3 +53,26 @@ def collect_hplc(sample_name,exp):#, CV=24, flowrate=0.5)
     updata_metadata()
     RE(hplc_scan(detectors=[pil1M, pilW1, pilW2, em1, em2], monitors=[]))
 
+
+## Plotting instructions
+## Method 1 : Using Best Effort Callback
+## enable live table
+#bec.enable_table()
+## enable plots
+#bec.enable_plots()
+
+
+## add hints for ex:
+#ss2.y.hints = {'fields': ['ss2_y', 'ss2_y_user_setpoint']}
+
+## use em1.describe() to find the fields you can use, then choose them with:
+#em1.hints = {'fields': ['em1_current1_mean_value', 'em1_current2_mean_value']}
+
+#RE(relative_scan(DETS, ss2.y, -1,1, 10))
+
+
+## Method 1 : Directly using LiveTable
+#bec.disable_table()
+#bec.disable_plots()
+## run RE on LiveTable with all the field names
+#RE(relative_scan(DETS, ss2.y, -1,1, 10), LiveTable(['ss2_y', 'ss2_y_user_setpoint']))
