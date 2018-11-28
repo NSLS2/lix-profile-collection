3ed63fcb7f3e6ceb52efe11c2fdf6fe20c869aff
LIX Operator <xf16id@xf16id-ws1.cs.nsls2.local>
2018-11-27 20:02:20 -0500

    databroker syntax update

--- startup/32-data.py
+++ startup/32-data.py
@@ -6,7 +6,7 @@ last_scan_id = None
 def fetch_scan(**kwargs):
     if len(kwargs) == 0:  # Retrieve last dataset
         header = db[-1]
-        return header, db.get_table(header, fill=True)
+        return header, header.table(fill=True)
     else:
         headers = db(**kwargs)
         return headers, db.get_table(headers, fill=True)
