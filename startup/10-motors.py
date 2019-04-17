0790e313af30d6463b15bd5e3a193c19630fd888
LIX Operator <xf16id@xf16id-ws1.cs.nsls2.local>
2019-04-17 08:54:09 -0400

    some shuffling-around and cleaning-up

--- startup/10-motors.py
+++ startup/10-motors.py
@@ -4,45 +4,34 @@ from ophyd import (PseudoPositioner, PseudoSingle)
 from epics import caget
 
 class XYMotor(Device):
-	x = Cpt(EpicsMotor, '-Ax:X}Mtr')
-	y = Cpt(EpicsMotor, '-Ax:Y}Mtr')
+    x = Cpt(EpicsMotor, '-Ax:X}Mtr')
+    y = Cpt(EpicsMotor, '-Ax:Y}Mtr')
+
+class XYZMotor(XYMotor):
+    z = Cpt(EpicsMotor, '-Ax:Z}Mtr')
+
+class XYPitchMotor(XYMotor):
+    pitch = Cpt(EpicsMotor, '-Ax:P}Mtr')
 
 """ note that the piezo slits PVs are incorrectly named X/Y, instead of dX/dY
 """ 
 class ApertureDev(Device):
-	dx = Cpt(EpicsMotor, '-Ax:dX}Mtr')
-	dy = Cpt(EpicsMotor, '-Ax:dY}Mtr')
+    dx = Cpt(EpicsMotor, '-Ax:dX}Mtr')
+    dy = Cpt(EpicsMotor, '-Ax:dY}Mtr')
     
-class XYZMotor(XYMotor):
-	z = Cpt(EpicsMotor, '-Ax:Z}Mtr')
-
-class XYPitchMotor(XYMotor):
-	pitch = Cpt(EpicsMotor, '-Ax:P}Mtr')
-
-class MonoDCM(Device):
-	bragg = Cpt(EpicsMotor, '-Ax:Bragg}Mtr')
-	x = Cpt(EpicsMotor, '-Ax:X}Mtr')
-	y = Cpt(EpicsMotor, '-Ax:Of2}Mtr')
-	pitch2 = Cpt(EpicsMotor, '-Ax:P2}Mtr')
-	roll2 = Cpt(EpicsMotor, '-Ax:R2}Mtr')
-	fine_pitch = Cpt(EpicsMotor, '-Ax:PF2}Mtr')
-	ccm_fine_pitch = Cpt(EpicsMotor, '-Ax:CCM_PF}Mtr')
-	pitch2_rb = Cpt(EpicsSignalRO, '-Ax:PF_RDBK}Mtr.RBV')
-
 class KBMirrorHorizontal(Device):
-	x1 = Cpt(EpicsMotor, '-Ax:XU}Mtr')
-	x2 = Cpt(EpicsMotor, '-Ax:XD}Mtr')
-	y1 = Cpt(EpicsMotor, '-Ax:YU}Mtr')
-	y2 = Cpt(EpicsMotor, '-Ax:YD}Mtr')
-	fine_pitch = Cpt(EpicsMotor, '-Ax:PF}Mtr')
-	pitch_rb = Cpt(EpicsSignalRO, '-Ax:PF_RDBK}Mtr.RBV')
+    x1 = Cpt(EpicsMotor, '-Ax:XU}Mtr')
+    x2 = Cpt(EpicsMotor, '-Ax:XD}Mtr')
+    y1 = Cpt(EpicsMotor, '-Ax:YU}Mtr')
+    y2 = Cpt(EpicsMotor, '-Ax:YD}Mtr')
+    pitch_rb = Cpt(EpicsSignalRO, '-Ax:PF_RDBK}Mtr.RBV')
 
 class KBMirrorVertical(Device):
-	x = Cpt(EpicsMotor, '-Ax:X}Mtr')
-	y1 = Cpt(EpicsMotor, '-Ax:YU}Mtr')
-	y2 = Cpt(EpicsMotor, '-Ax:YD}Mtr')
-	fine_pitch = Cpt(EpicsMotor, '-Ax:PF}Mtr')
-	pitch_rb = Cpt(EpicsSignalRO, '-Ax:PF_RDBK}Mtr.RBV')
+    x = Cpt(EpicsMotor, '-Ax:X}Mtr')
+    y1 = Cpt(EpicsMotor, '-Ax:YU}Mtr')
+    y2 = Cpt(EpicsMotor, '-Ax:YD}Mtr')
+    fine_pitch = Cpt(EpicsMotor, '-Ax:PF}Mtr')
+    pitch_rb = Cpt(EpicsSignalRO, '-Ax:PF_RDBK}Mtr.RBV')
 
 class Blades(Device):
     top = Cpt(EpicsMotor, '-Ax:T}Mtr')
@@ -51,69 +40,41 @@ class Blades(Device):
     inboard = Cpt(EpicsMotor, '-Ax:I}Mtr')
 
 class SlitsCenterAndGap(Device):
-	x = Cpt(EpicsMotor, '-Ax:X}Mtr')
-	dx = Cpt(EpicsMotor, '-Ax:dX}Mtr')
-	y = Cpt(EpicsMotor, '-Ax:Y}Mtr')
-	dy = Cpt(EpicsMotor, '-Ax:dY}Mtr')
+    x = Cpt(EpicsMotor, '-Ax:X}Mtr')
+    dx = Cpt(EpicsMotor, '-Ax:dX}Mtr')
+    y = Cpt(EpicsMotor, '-Ax:Y}Mtr')
+    dy = Cpt(EpicsMotor, '-Ax:dY}Mtr')
 
 class HRM1(Device):
-	y = Cpt(EpicsMotor, '-Ax:Y}Mtr')
-	pitch = Cpt(EpicsMotor, '-Ax:Th}Mtr')
+    y = Cpt(EpicsMotor, '-Ax:Y}Mtr')
+    pitch = Cpt(EpicsMotor, '-Ax:Th}Mtr')
 
 class HRM2(Device):
-	y = Cpt(EpicsMotor, '-Ax:Y}Mtr')
-	pitch = Cpt(EpicsMotor, '-Ax:Th}Mtr')
-	# Upstream
-	bend1 = Cpt(EpicsMotor, '-Ax:BU}Mtr')
-	# Downstream
-	bend2 = Cpt(EpicsMotor, '-Ax:BD}Mtr')
+    y = Cpt(EpicsMotor, '-Ax:Y}Mtr')
+    pitch = Cpt(EpicsMotor, '-Ax:Th}Mtr')
+    # Upstream
+    bend1 = Cpt(EpicsMotor, '-Ax:BU}Mtr')
+    # Downstream
+    bend2 = Cpt(EpicsMotor, '-Ax:BD}Mtr')
 
 class StageScan(Device):
-	x = Cpt(EpicsMotor, '-Ax:X}Mtr')
-	y = Cpt(EpicsMotor, '-Ax:Y}Mtr')
-	r = Cpt(EpicsMotor, '-Ax:Rot}Mtr')
+    x = Cpt(EpicsMotor, '-Ax:X}Mtr')
+    y = Cpt(EpicsMotor, '-Ax:Y}Mtr')
+    r = Cpt(EpicsMotor, '-Ax:Rot}Mtr')
     
 class Tilt(Device):
-	rx = Cpt(EpicsMotor, '-Ax:RX}Mtr')
-	ry = Cpt(EpicsMotor, '-Ax:RY}Mtr')
+    rx = Cpt(EpicsMotor, '-Ax:RX}Mtr')
+    ry = Cpt(EpicsMotor, '-Ax:RY}Mtr')
 
 
 class Microscope(Device):
-	x = Cpt(EpicsMotor, '-Ax:X}Mtr')
-	y = Cpt(EpicsMotor, '-Ax:Y}Mtr')
-	rx = Cpt(EpicsMotor, '-Ax:Rx}Mtr')
-	ry = Cpt(EpicsMotor, '-Ax:Ry}Mtr')
-	focus = Cpt(EpicsMotor, '-Ax:F}Mtr')
-	polarizer = Cpt(EpicsMotor, '-Ax:Pol}Mtr')
-	zoom = Cpt(EpicsMotor, '-Ax:Zm}Mtr')
-
-class WAXSBeamstop(XYMotor):
-	phi = Cpt(EpicsMotor, '-Ax:Z}Mtr')
-    
-class SAXSBeamstop(XYMotor):
-	r1 = Cpt(EpicsMotor, '-Ax:R1}Mtr')
-	r2 = Cpt(EpicsMotor, '-Ax:R2}Mtr')
-	r3 = Cpt(EpicsMotor, '-Ax:R3}Mtr')
-	x = Cpt(EpicsMotor, '-Ax:X}Mtr')
-	y = Cpt(EpicsMotor, '-Ax:Y}Mtr')
-    
-class ScanningStack1(Device):
-    x = Cpt(EpicsMotor, 'Ax:X}Mtr')
-    y = Cpt(EpicsMotor, 'Ax:Y}Mtr')
-
-class WAXS1(Device):
-    x=Cpt(EpicsMotor, 'Ax:X}Mtr')
-    y=Cpt(EpicsMotor, 'Ax:Y}Mtr')
-    z=Cpt(EpicsMotor, 'Ax:Z}Mtr')      
-          
-class WAXS2(Device):
-    x=Cpt(EpicsMotor, 'Ax:X}Mtr')
-    y=Cpt(EpicsMotor, 'Ax:Y}Mtr')
-    z=Cpt(EpicsMotor, 'Ax:Z}Mtr')
- 
-class StageScanF(Device):
-	x = Cpt(EpicsMotor, '-Ax:X}Mtr')
-	y = Cpt(EpicsMotor, '-Ax:Y}Mtr')
+    x = Cpt(EpicsMotor, '-Ax:X}Mtr')
+    y = Cpt(EpicsMotor, '-Ax:Y}Mtr')
+    rx = Cpt(EpicsMotor, '-Ax:Rx}Mtr')
+    ry = Cpt(EpicsMotor, '-Ax:Ry}Mtr')
+    focus = Cpt(EpicsMotor, '-Ax:F}Mtr')
+    polarizer = Cpt(EpicsMotor, '-Ax:Pol}Mtr')
+    zoom = Cpt(EpicsMotor, '-Ax:Zm}Mtr')
     
 class Screen(Device):
     y=Cpt(EpicsMotor, '-Ax:Y}Mtr')
@@ -124,9 +85,6 @@ class Screen(Device):
 
 ## White Beam Mirror
 wbm = XYPitchMotor('XF:16IDA-OP{Mir:WBM', name='wbm')
-
-## Si(111) Mono DCM
-mono = MonoDCM("XF:16IDA-OP{Mono:DCM", name="dcm")
 
 ## Beam-viewing screen #3
 scn3y = EpicsMotor('XF:16IDA-BI{FS:3-Ax:Y}Mtr', name='scn3y')
@@ -145,7 +103,7 @@ mps = Blades('XF:16IDA-OP{Slt:1', name='mps')
 
 
 #######################################################
-### LIX Secondary Source Enclosure SOE Hutch B
+### LIX Secondary Source Enclosure Hutch B
 #######################################################
 
 ## Beam Position Monitor
@@ -155,7 +113,7 @@ bpm2_pos = XYMotor('XF:16IDC-BI{BPM:2', name='bpm2')
 
 ## Secondary Source Aperture (SSA)
 #ssa = ApertureDev('XF:16IDB-OP{Slt:SSA', name='ssa')
-ssa1 = ApertureDev('XF:16IDB-OP{Slt:SSA1', name='ssa')
+ssa1 = ApertureDev('XF:16IDB-OP{Slt:SSA1', name='ssa1')
 
 
 ## Attenuator
@@ -167,14 +125,13 @@ atn2x = EpicsMotor('XF:16IDB-OP{Fltr:Atn-Ax:X2}Mtr', name='atn2x')
 atn3x = EpicsMotor('XF:16IDB-OP{Fltr:Atn-Ax:X3}Mtr', name='atn3x')
 
 ## Alternative SSA
-assa = SlitsCenterAndGap('XF:16IDB-OP{Slt:aSSA', name="aSSA")
+#assa = SlitsCenterAndGap('XF:16IDB-OP{Slt:aSSA', name="aSSA")
 
 ## Visual Beam Monitor (VBM)
 # Focus
-vbm_focus = EpicsMotor('XF:16IDB-BI{FS:VBM-Ax:F}Mtr', name='vbm_focus')
+#vbm_focus = EpicsMotor('XF:16IDB-BI{FS:VBM-Ax:F}Mtr', name='vbm_focus')
 # Zoom
-vbm_zoom = EpicsMotor('XF:16IDB-BI{FS:VBM-Ax:Zm}Mtr', name='vbm_zoom')
-
+#vbm_zoom = EpicsMotor('XF:16IDB-BI{FS:VBM-Ax:Zm}Mtr', name='vbm_zoom')
 
 #######################################################
 ### LIX Experimental End Station Enclosure EESE Hutch C
@@ -198,65 +155,20 @@ sg1 = SlitsCenterAndGap('XF:16IDC-OP{Slt:G1', name='sg1')
 ## Guard Slits 2
 sg2 = SlitsCenterAndGap('XF:16IDC-OP{Slt:G2', name='sg2')
 
-## guard slits on the solution scattering module
-sgs = SlitsCenterAndGap('XF:16IDC-OP{Slt:Gs', name='sgs')
-
-#########################################
-## In-air transmission measurement module
-#########################################
-
-## Coarse-resolution scanning
-smc = XYMotor('XF:16IDC-ES:InAir{Stg:ScanC', name='smc')
-
-## Fine-resolution scanning
-smf = XYMotor('XF:16IDC-ES:InAir{Stg:ScanF', name='smf')
-
-## Microscope
-microscope = Microscope('XF:16IDC-ES:InAir{Mscp:1', name='microscope')
-
-## Tilt
-tilt = Tilt('XF:16IDC-ES{Tilt', name='tilt')
-
-#########################################
-## Scanning Stack 1--Coarse Stage
-#########################################
-#ss1 = ScanningStack1('XF:16IDC-ES:InAir{Stg:ScanC-', name='ss1')
-
-
-#########################################
-## In-vaccum GISAXS/GID module
-#########################################
-
-## Guard Slits 2 in VAC
-# TODO: Review this motor
-#sg2dx = EpicsMotor('XF:16IDC-ES:GI{Slt:G2-Ax:X}Mtr', name='sg2dx')
-#sg2dy = EpicsMotor('XF:16IDC-ES:GI{Slt:G2-Ax:dX}Mtr', name='sg2dy')
-
-
 #########################################
 ## Detector System
 #########################################
 
-## WAXS Detector Positioning Stage 1
+## Detector Positioning Stages 
+saxs = XYZMotor('XF:16IDC-ES{Stg:SAXS', name='saxs')
 waxs1 = XYZMotor('XF:16IDC-ES{Stg:WAXS1', name='waxs1')
-
-## WAXS Detector Positioning Stage 2
 waxs2 = XYZMotor('XF:16IDC-ES{Stg:WAXS2', name='waxs2')
 
-## WAXS Beamstop
-wbs = WAXSBeamstop('XF:16IDC-ES{BS:WAXS', name='wbs')
-
-## SAXS Detector Positioning Stage
-saxs = XYZMotor('XF:16IDC-ES{Stg:SAXS', name='saxs')
-
-## WAXS Detector Positioning Stage
-waxs1 = WAXS1('XF:16IDC-ES{Stg:WAXS1-', name='waxs1')
-waxs2 = WAXS2('XF:16IDC-ES{Stg:WAXS2-', name='waxs2')
-
 ## SAXS Beamstop
-sbs = SAXSBeamstop('XF:16IDC-ES{BS:SAXS', name='sbs')
+sbs = XYMotor('XF:16IDC-ES{BS:SAXS', name='sbs')
 
 ## screen
 screen_SS = Screen('XF:16IDB-BI{SCN:SS',name='screen_ss')
 screen_SF = Screen('XF:16IDC-BI{FS:SF',name='screen_sf')
 
+microscope = Microscope('XF:16IDC-ES:InAir{Mscp:1', name='microscope')
--- startup/12-shutter.py
+++ startup/12-shutter.py
@@ -4,6 +4,7 @@ from ophyd import (EpicsSignal, Device, Component as Cpt)
 #import os
 #os.environ['EPICS_CA_ADDR_LIST']='10.16.2.59 10.16.2.60 10.16.2.61'
 
+
 class FastShutter(Device):
     OPEN_SHUTTER = "open"
     CLOSE_SHUTTER = "closed"
@@ -30,25 +31,6 @@ class FastShutter(Device):
         self.output.set(FastShutter.CLOSE_SHUTTER, settle_time=FastShutter.SETTLE_TIME)
         print(" done.")
 
-
-fast_shutter = FastShutter('XF:16IDB-BI', name='fast_shutter')
-
-
-class Scintillator(Device):
-    OPEN_SHUTTER = "open"
-    CLOSE_SHUTTER = "closed"
-    SETTLE_TIME = 0.1  # seconds
-    output = Cpt(EpicsSignal,'{shutter:2}sts', string=True, put_complete=True)
-
-    def open(self):
-        self.output.set(Scintillator.OPEN_SHUTTER, settle_time=Scintillator.SETTLE_TIME)
-
-    def close(self):
-        self.output.set(Scintillator.CLOSE_SHUTTER, settle_time=Scintillator.SETTLE_TIME)
-
-
-scintillator_shutter = Scintillator('XF:16IDB-BI', name='scintillator_shutter')
-
 class PhotonShutter(Device):
     OPEN_SHUTTER = "Open"
     CLOSE_SHUTTER = "Not Open"
@@ -59,41 +41,6 @@ class PhotonShutter(Device):
         
     def close(self):
         self.output.set(Scintillator.CLOSE_SHUTTER)
-            
+          
+fast_shutter = FastShutter('XF:16IDB-BI', name='fast_shutter')
 photon_shutter = PhotonShutter('XF:16IDA-PPS', name='photon_shutter')
-
-
-
-def one_nd_step_with_shutter(detectors, step, pos_cache):
-    """
-    Inner loop of an N-dimensional step scan
-
-    This is the default function for ``per_step`` param`` in ND plans.
-
-    Parameters
-    ----------
-    detectors : iterable
-        devices to read
-    step : dict
-        mapping motors to positions in this step
-    pos_cache : dict
-        mapping motors to their last-set positions
-    """
-    def move():
-        yield Msg('checkpoint')
-        grp = bp._short_uid('set')
-        for motor, pos in step.items():
-            if pos == pos_cache[motor]:
-                # This step does not move this motor.
-                continue
-            yield Msg('set', motor, pos, group=grp)
-            pos_cache[motor] = pos
-        yield Msg('wait', None, group=grp)
-
-    motors = step.keys()
-    yield from move()
-    yield from bp.abs_set(fast_shutter.output, FastShutter.OPEN_SHUTTER, settle_time=FastShutter.SETTLE_TIME, wait=True)
-    yield from trigger_and_read(list(detectors) + list(motors))
-    yield from bp.abs_set(fast_shutter.output, FastShutter.CLOSE_SHUTTER, settle_time=FastShutter.SETTLE_TIME, wait=True)
-        
-        
