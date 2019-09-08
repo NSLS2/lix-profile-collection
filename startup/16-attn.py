eb2dee2a6cff622d298d7a19f9e5b1f2ce42dffe
Maksim Rakitin <mrakitin@bnl.gov>
2019-09-08 15:23:06 -0400

    Enable Azure Pipelines integration

--- startup/16-attn.py
+++ startup/16-attn.py
@@ -1,12 +1,16 @@
+import os
 from ophyd import (EpicsSignal, Device, Component as Cpt)
 from time import sleep
 import threading
 from epics import PV
 
-at = np.genfromtxt("/GPFS/xf16id/controls/data_files/attn_comb",skip_header=0)
-t1p = np.genfromtxt("/GPFS/xf16id/controls/data_files/atn1x_pos_per.dat",delimiter=',',skip_header=0)
-t2p = np.genfromtxt("/GPFS/xf16id/controls/data_files/atn2x_pos_per.dat",delimiter=',',skip_header=0)
-t3p = np.genfromtxt("/GPFS/xf16id/controls/data_files/atn3x_pos_per.dat",delimiter=',',skip_header=0)
+
+profile_dir = get_ipython().profile_dir.location
+
+at = np.genfromtxt(os.path.join(profile_dir, "data_files/attn_comb"),skip_header=0)
+t1p = np.genfromtxt(os.path.join(profile_dir, "data_files/atn1x_pos_per.dat"),delimiter=',',skip_header=0)
+t2p = np.genfromtxt(os.path.join(profile_dir, "data_files/atn2x_pos_per.dat"),delimiter=',',skip_header=0)
+t3p = np.genfromtxt(os.path.join(profile_dir, "data_files/atn3x_pos_per.dat"),delimiter=',',skip_header=0)
 
 
 class Attenuator():
