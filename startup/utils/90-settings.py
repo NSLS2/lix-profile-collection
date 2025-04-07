cb42c66f9d3124dc8629cc94debe61a58f7707b0
Lin Yang <lyang@bnl.gov>
2025-04-07 14:43:52 -0400

    send meta data to redis for live data processing

--- startup/utils/90-settings.py
+++ startup/utils/90-settings.py
@@ -1,15 +1,42 @@
 print(f"Loading {__file__}...")
+import json
 
+from collections import OrderedDict
+import redis
 
 def print_scanid(name, doc):
     global last_scan_uid
     global last_scan_id
+
+    header_attrs = ['proc_path', 'sample_name', 'plan_name']
+    
     if name == 'start':
         last_scan_uid = doc['uid']
         last_scan_id = doc['scan_id']  
         print('Scan ID:', doc['scan_id'])
         print('Unique ID:', doc['uid'])
-        pil.update_header(doc['uid'])
+        
+        hdr_dict = {"uid": doc['uid'], 
+                    #"proc_path": proc_path,
+                    #"sample_name": current_sample
+                   }
+        #pil.update_header(json.dumps(hdr_dict))
+        pil.update_header(f"uid={doc['uid']}")
+        
+        # also update header info in Redis
+        with redis.Redis(host=redis_host, port=redis_port, db=0) as r:
+            hdrs = r.get('scan_info')
+            if hdrs==None: # no info stored yet
+                hdrs = OrderedDict({})
+            else:
+                hdrs = json.loads(hdrs, object_pairs_hook=OrderedDict)
+                
+            h = db[-1].start
+            hdrs[h['uid']] = {k: h[k] for k in header_attrs}
+            uids = list(hdrs.keys())
+            if len(uids)>3: # keep up to 3 scans
+                hdrs.popitem(last=False)
+            r.set("scan_info", json.dumps(hdrs))
        
 def print_scanid_stop(name, doc):
     global last_scan_uid
