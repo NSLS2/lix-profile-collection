50c5217356f0b6583ead7a9af8150ffe9209aed5
Lin Yang <lyang@bnl.gov>
2026-03-17 11:31:00 -0400

    add traj for ss.ry

--- startup/experiments/startup_scanning.py
+++ startup/experiments/startup_scanning.py
@@ -9,9 +9,11 @@ caput(ss.xc.prefix+".DIR", 1)
 
 ss.x = xps.def_motor("scan.X", "ss_x", direction=-1)
 ss.y = xps.def_motor("scan.Y", "ss_y")
+xps_traj = XPStraj(xps, "scan")
+
 if "rot" in xps.groups.keys():
     ss.ry = xps.def_motor("rot.rY", "ss_ry")
-xps_traj = XPStraj(xps, "scan")
+    phi_traj = XPStraj(xps, "rot")
 
 # fix dir/res of SmarAct gonio 
 caput(ss.sx.prefix+".DIR", 1)
